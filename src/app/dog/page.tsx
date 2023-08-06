4021ddfc92739b5001a44bf40d93ff64e6fbd2cc
JavierCanoy <javier@mabolotech.com>
2023-08-06 20:38:09 +0800

    pratis-loading-pages

--- src/app/dog/page.tsx
+++ src/app/dog/page.tsx
@@ -7,16 +7,22 @@ import Image from "next/image";
 
 //   return res.json();
 // }
+// setTimeout(() => {
+//   return res.json();
+// }, 3000); // 3 seconds
 
 const getDog = async () => {
   const res = await fetch("https://dog.ceo/api/breeds/image/random", {
     cache: "no-cache",
   });
-  return res.json();
+  const data = res.json();
+
+  return data;
 };
 
 export default async function Dogs() {
   const data = await getDog();
+  console.log("data is here ! ");
   return (
     <div className=" flex justify-center mb-20">
       <Image src={data.message} width={200} height={200} alt="dog" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,53 @@
+"use client";
 import Image from "next/image";
 import Users from "./users/page";
 import Dogs from "./dog/page";
 import { Suspense } from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { useState, CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 export default function Home() {
+  const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#ffffff");
+  // const form = useRef();
+
+  // const sendEmail = (e: any) => {
+  //   e.preventDefault();
+  //   emailjs
+  //     .sendForm(
+  //       "service_hnu7x0u",
+  //       "template_7y8qbag",
+  //       form.current,
+  //       "BLDAtPUmTlrNgOSm8"
+  //     )
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
+  //         console.log("send ! ");
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //       }
+  //     );
+  // };
   return (
     <main>
+      {/* <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>  */}
+
       <div>
         <p className=" text-center">javier bernadas canoy ! </p>
         <div>
@@ -30,26 +73,153 @@ export default function Home() {
             <Users />
           </Suspense>
         </div>
-        <div>
-          <h1 className=" text-center"> Dogs ! </h1>
+        <div className=" flex justify-center">
           <Suspense
             fallback={
-              <div
-                role="status"
-                className=" flex justify-center mb-20 space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center"
-              >
-                <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
-                  <svg
-                    className="w-10 h-10 text-gray-200 dark:text-gray-600"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 18"
-                  >
-                    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-                  </svg>
-                </div>
+              // <div>
+              //   <svg
+              //     xmlns="http://www.w3.org/2000/svg"
+              //     height="50px"
+              //     width="50px"
+              //     viewBox="0 0 200 200"
+              //     class="pencil"
+              //   >
+              //     <defs>
+              //       <clipPath id="pencil-eraser">
+              //         <rect height="30" width="30" ry="5" rx="5"></rect>
+              //       </clipPath>
+              //     </defs>
+              //     <circle
+              //       transform="rotate(-113,100,100)"
+              //       stroke-linecap="round"
+              //       stroke-dashoffset="439.82"
+              //       stroke-dasharray="439.82 439.82"
+              //       stroke-width="2"
+              //       stroke="currentColor"
+              //       fill="none"
+              //       r="70"
+              //       class="pencil__stroke"
+              //     ></circle>
+              //     <g transform="translate(100,100)" class="pencil__rotate">
+              //       <g fill="none">
+              //         <circle
+              //           transform="rotate(-90)"
+              //           stroke-dashoffset="402"
+              //           stroke-dasharray="402.12 402.12"
+              //           stroke-width="30"
+              //           stroke="hsl(223,90%,50%)"
+              //           r="64"
+              //           class="pencil__body1"
+              //         ></circle>
+              //         <circle
+              //           transform="rotate(-90)"
+              //           stroke-dashoffset="465"
+              //           stroke-dasharray="464.96 464.96"
+              //           stroke-width="10"
+              //           stroke="hsl(223,90%,60%)"
+              //           r="74"
+              //           class="pencil__body2"
+              //         ></circle>
+              //         <circle
+              //           transform="rotate(-90)"
+              //           stroke-dashoffset="339"
+              //           stroke-dasharray="339.29 339.29"
+              //           stroke-width="10"
+              //           stroke="hsl(223,90%,40%)"
+              //           r="54"
+              //           class="pencil__body3"
+              //         ></circle>
+              //       </g>
+              //       <g
+              //         transform="rotate(-90) translate(49,0)"
+              //         class="pencil__eraser"
+              //       >
+              //         <g class="pencil__eraser-skew">
+              //           <rect
+              //             height="30"
+              //             width="30"
+              //             ry="5"
+              //             rx="5"
+              //             fill="hsl(223,90%,70%)"
+              //           ></rect>
+              //           <rect
+              //             clip-path="url(#pencil-eraser)"
+              //             height="30"
+              //             width="5"
+              //             fill="hsl(223,90%,60%)"
+              //           ></rect>
+              //           <rect
+              //             height="20"
+              //             width="30"
+              //             fill="hsl(223,10%,90%)"
+              //           ></rect>
+              //           <rect
+              //             height="20"
+              //             width="15"
+              //             fill="hsl(223,10%,70%)"
+              //           ></rect>
+              //           <rect
+              //             height="20"
+              //             width="5"
+              //             fill="hsl(223,10%,80%)"
+              //           ></rect>
+              //           <rect
+              //             height="2"
+              //             width="30"
+              //             y="6"
+              //             fill="hsla(223,10%,10%,0.2)"
+              //           ></rect>
+              //           <rect
+              //             height="2"
+              //             width="30"
+              //             y="13"
+              //             fill="hsla(223,10%,10%,0.2)"
+              //           ></rect>
+              //         </g>
+              //       </g>
+              //       <g
+              //         transform="rotate(-90) translate(49,-30)"
+              //         class="pencil__point"
+              //       >
+              //         <polygon
+              //           points="15 0,30 30,0 30"
+              //           fill="hsl(33,90%,70%)"
+              //         ></polygon>
+              //         <polygon
+              //           points="15 0,6 30,0 30"
+              //           fill="hsl(33,90%,50%)"
+              //         ></polygon>
+              //         <polygon
+              //           points="15 0,20 10,10 10"
+              //           fill="hsl(223,10%,10%)"
+              //         ></polygon>
+              //       </g>
+              //     </g>
+              //   </svg>
+              // </div>
+              <div class=" mb-20 flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+                <svg
+                  class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 18"
+                >
+                  <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                </svg>
               </div>
+
+              //loading-spinner
+              // <div>
+              //   <ClipLoader
+              //     color={color}
+              //     loading={loading}
+              //     cssOverride={override}
+              //     size={150}
+              //     aria-label="Loading Spinner"
+              //     data-testid="loader"
+              //   />
+              // </div>
             }
           >
             <Dogs />
